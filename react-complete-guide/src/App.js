13366bba8ce0fc87d7e787334ba4302707d119f2
Ernie_py <user@example.com>
2023-02-04 10:08:38 -0500

    Added Let'sget started header

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h2> Let's get started </h2>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
