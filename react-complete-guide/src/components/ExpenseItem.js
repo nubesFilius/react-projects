d54f618efcfa6becfbdeaecb8736d16a48d86817
Ernie_py <user@example.com>
2023-02-04 11:24:44 -0500

    Lil beautifying of Expense item component with CSS

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,6 +1,16 @@
+import './ExpenseItem.css';
+
 // Convention is to repeat file name
 function ExpenseItem() {
-  return <h2> Expense item! </h2>;
+  return (
+    <div className="expense-item">
+      <div>February 3rd 2023</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$160</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
