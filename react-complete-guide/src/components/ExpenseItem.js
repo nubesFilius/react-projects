f07ab4b576620449cc676721f13e1d40776c6b9a
Ernesto Perez La Rosa <eperezlarosa@cisco.com>
2023-02-05 16:18:56 -0500

    Introduce state to update the title value on a click

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,15 +1,24 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 
 // Convention is to repeat file name
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  function clickHandler() {
+    setTitle('NewTitle');
+    console.log('Updated title!');
+  }
+
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}> Do something</button>
     </div>
   );
 }
