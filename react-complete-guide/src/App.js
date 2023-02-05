515a98cb115df1a7553c90634fd36c14ac68b9af
Ernesto Perez La Rosa <eperezlarosa@cisco.com>
2023-02-05 17:26:00 -0500

    Adding new expense form and re-org the folders and files

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import ExpenseItem from './components/ExpenseItem';
-import './App.css';
+import React from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const expenses = [
@@ -26,45 +27,9 @@ function App() {
   ];
 
   return (
-    <div className="App">
-      <div>
-        <h2>
-          <ExpenseItem
-            title={expenses[0].title}
-            amount={expenses[0].amount}
-            date={expenses[0].date}
-          />
-          <ExpenseItem
-            title={expenses[1].title}
-            amount={expenses[1].amount}
-            date={expenses[1].date}
-          />
-          <ExpenseItem
-            title={expenses[2].title}
-            amount={expenses[2].amount}
-            date={expenses[2].date}
-          />
-          <ExpenseItem
-            title={expenses[3].title}
-            amount={expenses[3].amount}
-            date={expenses[3].date}
-          />
-        </h2>
-      </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
 }
